2e7be415accac751e3736cec1e1fd4b742f26f09
Michiel de Jong <michiel@unhosted.org>
2012-02-03 06:33:56 +0100

    cleanup

--- remoteStorage-0.2.0.js
+++ remoteStorage-0.2.0.js
@@ -1,9 +1,6 @@
 define(function(require) {
 
   require(['./controller'], function(controller) {
-    var options = {};
-    controller.onLoad(options);
-
     window.remoteStorage = {
       createClients: function(userAddress, receiverPageAddress, categories, cb) {
         return controller.createClients(userAddress, receverPageAddress, categories, cb);
